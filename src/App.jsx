396a545ce1e3c3f70d395339c0c45cd9b3f60ebd
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-07 21:09:00 +0600

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <h2 className="text-7xl text-center font-bold">Movie Hunter</h2>
       <h3 className="text-4xl my-20 text-center">
         Some Of Our Best Movies {movies.length}
-        <div className="grid grid-cols-3 my-14 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-14 mx-auto md:w-10/12 gap-10">
           {movies.map((movie, index) => (
             <MovieCard
               key={movie.show.id}
